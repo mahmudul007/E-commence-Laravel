4716b8aa8e0b6541077f2eab44bbb21a6de96118
mahmudul007 <sayedmahmudul05@gmail.com>
2023-02-19 17:39:10 +0600

    add migration

---
+++ database/migrations/2023_01_30_101419_create_products_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('sku');
+            $table->decimal('price');
+            $table->string('weight')->nullable();
+            $table->unsignedBigInteger('brand_id');
+            $table->unsignedBigInteger('catagory_id');
+            $table->longText('description');
+            $table->bigInteger('quantity');
+            $table->boolean('product_live')->default(false);
+            $table->boolean('unlimited_stock')->nullable()->default(false);
+            $table->timestamps();
+
+
+            $table->foreign('catagory_id')
+            ->references('id')->on('categories')
+            ->onDelete('cascade');
+            $table->foreign('brand_id')
+            ->references('id')->on('brands')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2023_01_30_131016_create_carts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('session_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('quantity');
+
+            $table->foreign('session_id')
+            ->references('id')->on('shopping_sessions')
+            ->onDelete('cascade');
+
+            $table->foreign('product_id')
+            ->references('id')->on('products')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+};
